2478860ed4f764ab64bb6a6139e75dc0a9fd913f
ankush <aptech631@gmail.com>
2024-07-28 17:52:12 +0530

    Initial commit

---
+++ static/js/test.js
@@ -0,0 +1,3 @@
+console.log("ankush");
+// isse hoga ye ki app reload per google me console per hme ankush milega likh k jisse pta chlega ki load ho ra 
+// iski jgh kuch v js code jod skte h ye to bas example k liye dali h hmne
